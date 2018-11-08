c58c5dc4d2ae71e4ce21853814412eb050ce86f8
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-08 13:37:18 +0530

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Ravi Inder Manshahia on 08/11/18.
+//  Copyright © 2018 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
